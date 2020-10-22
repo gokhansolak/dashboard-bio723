56990411f289498bf4a7974573065bcfa0622804
Gokhan Solak <gknsolak@gmail.com>
2020-10-22 16:48:22 +0100

    my commit

---
+++ Exercise8_aProc.py
@@ -0,0 +1,5 @@
+def aProc(first="spam", second="ham"):
+    """ This procedure has default values for its arguments """
+    print("The first argument is: ", first)
+    print("And the second is    : ", second)
+    print("---------------------------------")
